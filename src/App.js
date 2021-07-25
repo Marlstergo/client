2cf6e70dd5fcd5af9469cfc50fa9b1222fca4ad6
Marlstergo <abdulmaliqshola@gmail.com>
2021-07-25 07:30:02 +0100

    lol

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useReducer, useState } from "react";
+import React, { useReducer, useState } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./components/Home";
